076a005e386dc5800ad54f46405699df31e2f1fb
Tony Solomonik <tony.solomonik@gmail.com>
2021-02-06 14:52:13 +0200

    Init commit

---
+++ README.md
@@ -0,0 +1,1 @@
+Download episodes automatically from sdarot.tv
---
+++ main.py
@@ -0,0 +1,115 @@
+import asyncio
+import os
+import re
+import sys
+from collections import namedtuple
+from concurrent.futures.thread import ThreadPoolExecutor
+from contextlib import contextmanager
+
+import aiofiles
+import aiohttp
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.expected_conditions import visibility_of_element_located
+from selenium.webdriver.support.wait import WebDriverWait
+from tqdm import tqdm
+
+SITE = "https://sdarot.tv/watch"
+SHOW = "149-המשרד-ארה-ב-the-office-us"
+SEASONS = list(range(5, 10))
+
+EPISODE_REGEX = re.compile(r'<li data-episode="(\d+)"')
+NUMBER_OF_PARALLEL_MISSIONS = 4
+
+Mission = namedtuple('Mission', 'season episode url output')
+
+
+@contextmanager
+def quit_web(driver):
+    try:
+        yield driver
+    finally:
+        driver.quit()
+
+
+def load_episode(url):
+    with quit_web(webdriver.Firefox()) as driver:
+        driver.get(url)
+
+        WebDriverWait(driver, 40).until(visibility_of_element_located((By.ID, "afterLoad")))
+        proceed = driver.find_element_by_id("proceed")
+        proceed.click()
+
+        WebDriverWait(driver, 5).until(visibility_of_element_located((By.ID, "videojs_html5_api")))
+        video = driver.find_element_by_id("videojs_html5_api")
+        cookies = {cookie.get('name'): cookie.get('value') for cookie in driver.get_cookies()}
+        return video.get_attribute("src"), cookies
+
+
+async def download_video(url, output, **kwargs):
+    async with aiohttp.ClientSession(**kwargs) as session:
+        async with session.get(url) as response:
+            if not response.status == 200:
+                return False
+
+            async with aiofiles.open(output, mode='wb') as f:
+                async for data, _ in response.content.iter_chunks():
+                    await f.write(data)
+
+            return True
+
+
+async def get_episodes(url, **kwargs):
+    async with aiohttp.ClientSession(**kwargs) as session:
+        async with session.get(url) as response:
+            if not response.status == 200:
+                return []
+
+            text = await response.text()
+            return [int(i) for i in EPISODE_REGEX.findall(text)]
+
+
+def run_mission(mission):
+    async def run_mission_async():
+        loop = asyncio.get_running_loop()
+        try:
+            video_url, cookies = await loop.run_in_executor(None, load_episode, mission.url)
+        except Exception:
+            print(f'Failed to get episode url of season {mission.season} / episode {mission.episode}')
+            return
+
+        if not await download_video(video_url, mission.output, cookies=cookies):
+            print(f'Failed to download video of season {mission.season} / episode {mission.episode}')
+
+    asyncio.new_event_loop().run_until_complete(run_mission_async())
+
+
+async def download():
+    missions = []
+    for season in SEASONS:
+        season_url = f'{SITE}/{SHOW}/season/{season}'
+        episodes = await get_episodes(season_url)
+        season_directory = f'Season_{season}'
+
+        if not os.path.exists(season_directory):
+            os.mkdir(season_directory)
+
+        for episode in episodes:
+            episode_url = f'{season_url}/episode/{episode}'
+            episode_output = os.path.join(season_directory, f'Episode_{episode}.mp4')
+            missions.append(Mission(season, episode, episode_url, episode_output))
+
+    with ThreadPoolExecutor(max_workers=NUMBER_OF_PARALLEL_MISSIONS) as pool, \
+            tqdm(total=len(missions), unit='ep') as progress_bar:
+        for _ in pool.map(run_mission, missions):
+            progress_bar.update(1)
+
+
+def main():
+    if sys.version_info[0] == 3 and sys.version_info[1] >= 8 and sys.platform.startswith('win'):
+        asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
+    asyncio.run(download())
+
+
+if __name__ == '__main__':
+    main()
