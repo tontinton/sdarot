f104fa5883c8b8fd2c7a6a7901d3ff49e147dbad
DoronZ <doron88@gmail.com>
2021-02-06 15:27:39 +0200

    fix pep8

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+#!/usr/bin/python3
 import asyncio
 import os
 import re
@@ -10,7 +11,8 @@ import aiofiles
 import aiohttp
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-from selenium.webdriver.support.expected_conditions import visibility_of_element_located
+from selenium.webdriver.support.expected_conditions import \
+    visibility_of_element_located
 from selenium.webdriver.support.wait import WebDriverWait
 from tqdm import tqdm
 
@@ -36,13 +38,16 @@ def load_episode(url):
     with quit_web(webdriver.Firefox()) as driver:
         driver.get(url)
 
-        WebDriverWait(driver, 40).until(visibility_of_element_located((By.ID, "afterLoad")))
+        WebDriverWait(driver, 40).until(
+            visibility_of_element_located((By.ID, "afterLoad")))
         proceed = driver.find_element_by_id("proceed")
         proceed.click()
 
-        WebDriverWait(driver, 5).until(visibility_of_element_located((By.ID, "videojs_html5_api")))
+        WebDriverWait(driver, 5).until(
+            visibility_of_element_located((By.ID, "videojs_html5_api")))
         video = driver.find_element_by_id("videojs_html5_api")
-        cookies = {cookie.get('name'): cookie.get('value') for cookie in driver.get_cookies()}
+        cookies = {cookie.get('name'): cookie.get('value') for cookie in
+                   driver.get_cookies()}
         return video.get_attribute("src"), cookies
 
 
@@ -73,13 +78,19 @@ def run_mission(mission):
     async def run_mission_async():
         loop = asyncio.get_running_loop()
         try:
-            video_url, cookies = await loop.run_in_executor(None, load_episode, mission.url)
+            video_url, cookies = await loop.run_in_executor(None, load_episode,
+                                                            mission.url)
         except Exception:
-            print(f'Failed to get episode url of season {mission.season} / episode {mission.episode}')
+            print(
+                f'Failed to get episode url of season {mission.season} / '
+                f'episode {mission.episode}')
             return
 
-        if not await download_video(video_url, mission.output, cookies=cookies):
-            print(f'Failed to download video of season {mission.season} / episode {mission.episode}')
+        if not await download_video(video_url, mission.output,
+                                    cookies=cookies):
+            print(
+                f'Failed to download video of season {mission.season} / '
+                f'episode {mission.episode}')
 
     asyncio.new_event_loop().run_until_complete(run_mission_async())
 
@@ -96,8 +107,10 @@ async def download():
 
         for episode in episodes:
             episode_url = f'{season_url}/episode/{episode}'
-            episode_output = os.path.join(season_directory, f'Episode_{episode}.mp4')
-            missions.append(Mission(season, episode, episode_url, episode_output))
+            episode_output = os.path.join(season_directory,
+                                          f'Episode_{episode}.mp4')
+            missions.append(
+                Mission(season, episode, episode_url, episode_output))
 
     with ThreadPoolExecutor(max_workers=NUMBER_OF_PARALLEL_MISSIONS) as pool, \
             tqdm(total=len(missions), unit='ep') as progress_bar:
@@ -106,7 +119,8 @@ async def download():
 
 
 def main():
-    if sys.version_info[0] == 3 and sys.version_info[1] >= 8 and sys.platform.startswith('win'):
+    if sys.version_info[0] == 3 and sys.version_info[
+        1] >= 8 and sys.platform.startswith('win'):
         asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
     asyncio.run(download())
 
